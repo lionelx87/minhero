0b462181d0dc7dcfc9992887063bcb7330464feb
Lionel Acosta <lionelx87@gmail.com>
2025-08-28 00:05:17 -0300

    feat(test): migrate from Karma to Jest

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'jsdom',
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  testMatch: [
+    '<rootDir>/src/**/*.spec.ts'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
+};
