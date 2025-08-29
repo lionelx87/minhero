b5c40651a231142edca119357b215db0fdedd93e
Lionel Acosta <lionelx87@gmail.com>
2025-08-29 13:22:36 -0300

    feat(home): apply styles in search box

--- src/app/core/components/search-box/search-box.component.ts
+++ src/app/core/components/search-box/search-box.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-search-box',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.scss',
 })
