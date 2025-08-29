427264acb5154cf22b73364e20b46bdcf6a61d73
Lionel Acosta <lionelx87@gmail.com>
2025-08-29 11:14:32 -0300

    feat(hero-form): adds back navigation

--- src/app/pages/hero-upsert/hero-upsert.component.ts
+++ src/app/pages/hero-upsert/hero-upsert.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 import { HeroFormComponent } from '@core/components';
 
 @Component({
   selector: 'app-hero-upsert',
-  imports: [HeroFormComponent],
+  imports: [HeroFormComponent, MatButtonModule, MatIconModule],
   templateUrl: './hero-upsert.component.html',
   styleUrl: './hero-upsert.component.scss',
 })
-export class HeroUpsertComponent {}
+export class HeroUpsertComponent {
+  private readonly router = inject(Router);
+
+  back(): void {
+    this.router.navigate(['/']);
+  }
+}
