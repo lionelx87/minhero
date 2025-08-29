3555165104262876589baf17007270d619071ab5
Lionel Acosta <lionelx87@gmail.com>
2025-08-28 21:20:27 -0300

    feat(app): create main layout

---
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { LayoutModule } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [RouterOutlet],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.scss',
+})
+export class MainLayoutComponent {}
