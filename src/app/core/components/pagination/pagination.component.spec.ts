ec58c51f2be772c389468d99bc1be7ea221c50cf
Lionel Acosta <lionelx87@gmail.com>
2025-08-31 21:36:00 -0300

    feat(home): implements pagination for heroes list

--- src/app/core/components/pagination/pagination.component.spec.ts
+++ src/app/core/components/pagination/pagination.component.spec.ts
@@ -6,14 +6,20 @@ describe('PaginationComponent', () => {
   let component: PaginationComponent;
   let fixture: ComponentFixture<PaginationComponent>;
 
+  const total = 10;
+  const pageIndex = 0;
+  const pageSize = 3;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PaginationComponent]
-    })
-    .compileComponents();
+      imports: [PaginationComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PaginationComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('total', total);
+    fixture.componentRef.setInput('pageIndex', pageIndex);
+    fixture.componentRef.setInput('pageSize', pageSize);
     fixture.detectChanges();
   });
 
